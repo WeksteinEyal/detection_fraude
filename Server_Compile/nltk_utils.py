c74b53b1ecf0d97697f9ed465c05ff07c2fa8828
Wekstein Eyal <154584735+WeksteinEyal@users.noreply.github.com>
2024-03-28 05:32:50 +0100

    Update nltk_utils.py

--- Server_Compile/nltk_utils.py
+++ Server_Compile/nltk_utils.py
@@ -1,14 +1,14 @@
 import nltk
 import numpy as np
 #nltk.download('punkt')
-from nltk.stem.porter import PorterStemmer
-stemmer = PorterStemmer()
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 
 def stem(word):
-    return stemmer.stem(word.lower())
+    return lemmatizer.lemmatize(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
     tokenized_sentence = [stem(w) for w in tokenized_sentence]
